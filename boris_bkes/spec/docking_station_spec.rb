69f90667b713f02ecb45ec6aaca36fcdd12d93e8
Nick Stoneman <nick.stoneman@gmail.com>
2015-05-21 19:15:46 +0100

    test provide the list of available bikes

--- boris_bkes/spec/docking_station_spec.rb
+++ boris_bkes/spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ require_relative  '../lib/bike'
 describe DockingStation do
   let(:station) { DockingStation.new(:capacity => 20) }
   let(:bike) { Bike.new }
-  
+
   def fill_station(station)
       20.times { station.dock(Bike.new) }
   end
@@ -34,6 +34,14 @@ describe DockingStation do
   it "should not accept a bike if it's full" do
       fill_station station
       expect { station.dock(bike) }.to raise_error RuntimeError
+  end
+
+  it "should provide the list of available bikes" do
+    working_bike, broken_bike = Bike.new, Bike.new
+    broken_bike.break
+    station.dock(working_bike)
+    station.dock(broken_bike)
+    expect(station.available_bikes).to eq([working_bike])
   end
 
 end
